d0238324b7b2b34d24a5b8ee7d4924f02d1733d6
ihsanannashir <64590661+ihsanannashir@users.noreply.github.com>
2023-06-06 15:32:27 +0700

    add fetch api table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
+  const [volcano, setVolcano] = useState([]);
+
+  const fetchData = () => {
+    fetch(process.env.REACT_APP_API)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setVolcano(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Table Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>nama</th>
+            <th>bentuk</th>
+            <th>tinggi_meter</th>
+            <th>estimasi_letusan_terakhir</th>
+            <th>geolokasi</th>
+          </tr>
+        </thead>
+        {volcano.map((item, idx) => {
+          return (
+            <tbody key={idx}>
+              <tr>
+                <td>{item.nama}</td>
+                <td>{item.bentuk}</td>
+                <td>{item.tinggi_meter}</td>
+                <td>{item.estimasi_letusan_terakhir}</td>
+                <td>{item.geolokasi}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
